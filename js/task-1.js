386d1b04f8dcd42f41391229844fda3a57e886e9
Larysa <seagull2610@gmail.com>
2024-09-19 21:39:19 +0200

    update

--- js/task-1.js
+++ js/task-1.js
@@ -1,13 +1,12 @@
 function makeTransaction (quantity, pricePerDroid) {
     const totalPrice = quantity * pricePerDroid;
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+    console.log(`You ordered ${quantity} droids worth ${totalPrice} credits!`);
+    return ;
 }
 
 
-console.log(makeTransaction(5, 3000));
-console.log(makeTransaction(3, 1000));
-console.log(makeTransaction(10, 500));
-
-
+makeTransaction(5, 3000);
+makeTransaction(3, 1000);
+makeTransaction(10, 500);
 
 
