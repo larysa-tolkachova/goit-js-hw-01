aac7ddff086daa745e61d2f057521eec4fd063c5
Larysa <seagull2610@gmail.com>
2024-09-20 12:05:40 +0200

    update

--- js/task-2.js
+++ js/task-2.js
@@ -5,5 +5,10 @@ function getShippingMessage (country, price, deliveryFee) {
 }
 
 getShippingMessage("Australia", 120, 50);
+console.log(getShippingMessage("Australia", 120, 50)); 
+
 getShippingMessage("Germany", 80, 20);
+console.log(getShippingMessage("Germany", 80, 20)); 
+
 getShippingMessage("Sweden", 100, 20);
+console.log(getShippingMessage("Sweden", 100, 20));
